db272f15e06c4bfe0abf4271584c804b74d22ceb
Marazzaite <55126149+Marazzaite@users.noreply.github.com>
2019-10-18 15:57:10 +0300

    Add files via upload

---
+++ php.php
@@ -0,0 +1,32 @@
+<?php
+
+$maistosarasas = [
+[
+'Kiausiniai' => [
+    'kiekis' => '2',
+    'dydis' => 'dideli',
+    ],
+],
+    
+[
+    'Baklazanai' => [
+    'kiekis' => '1',
+    'dydis' => 'mazas',
+    ]
+], 
+[
+    'Grietine' => [
+        'kiekis' => '1',
+        'dydis' => 'mazas',
+        ]
+    ]
+    ];
+
+    
+
+    
+
+
+var_dump($maistosarasas); 
+​
+?>
