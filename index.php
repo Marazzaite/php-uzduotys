7c3c793a3f3a82c6073f363170c882a2f5ff282c
Marazzaite <55126149+Marazzaite@users.noreply.github.com>
2019-10-18 16:54:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$sarasas = [
+    [
+    'name' => 'Petro Pizdzio',
+    'action' => 'Prap*stas telefonas',
+],
+    [
+    'name' => 'Tomo Ablomo',
+    'action' => 'naudojamas buljonas',
+],
+    [
+    'name' => 'Anos Shitkovos',
+    'action' => 'pasibaiges rulonas',
+    ]
+];
+
+    
+var_dump($sarasas); 
+​
+?>
